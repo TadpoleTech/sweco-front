747ee936936cc8feca8729449e764223d3b2917d
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-03-22 23:16:50 +0200

    feat: add button state

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,12 +2,25 @@ import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import "../styles/menu.css";
 
-function Menu() {
-    return (
-        <div className="hamburger-menu">
-            <RxHamburgerMenu />
-        </div>
-    )
+function Menu(props) {
+  let burger;
+  if (props.isOpen) {
+    burger =  <RxHamburgerMenu className="open"/>
+  } else {
+    burger =  <RxHamburgerMenu className="closed"/>
+  }
+
+  function onMenuClick() {
+    props.updateMenu(!props.isOpen);
+  }
+
+  return (
+    <div className="hamburger-menu">
+      <button onClick={onMenuClick}>
+        {burger}
+      </button>
+    </div>
+  );
 }
 
 export default Menu;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,16 @@
 import "../styles/home.css";
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../components/menu";
 import SearchBar from "../components/areaSearch";
 
 function Home() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
     <div className="home">
       <header className="header">
         <span>logo</span>
         <SearchBar/>
-        <Menu/>
+        <Menu isOpen={menuIsOpen} updateMenu={setMenuIsOpen}/>
       </header>
       <div className="content"></div>
     </div>
