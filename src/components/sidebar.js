25d51ca3c76802da8bed2fbeeb9575484e478106
16cats <97065659+16cats@users.noreply.github.com>
2024-03-23 16:21:38 +0200

    sign in/up js created, sidebar edited

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "../styles/sidebar.css";
+import { HiArrowSmRight, HiTable, HiUser, HiAnnotation } from 'react-icons/hi';
+import { Sidebar } from 'flowbite-react';
 
-function Sidebar() {
-    return (
-        <div>
-            <h1>
-                menu
-            </h1>
-        </div>
-    )
+function MenuSidebar() {
+  return (
+    <Sidebar aria-label="meow">
+      <Sidebar.Logo href="#" img="/favicon.svg" imgAlt="Flowbite logo">
+        UrbanPulseLogo
+      </Sidebar.Logo>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Profile
+          </Sidebar.Item>
+          <Sidebar.Collapse icon={HiAnnotation} label="History">
+          <Sidebar.Item href="pages/yourInformation.js">Information</Sidebar.Item>
+            <Sidebar.Item href="pages/yourPosts.js">Posts</Sidebar.Item>
+            <Sidebar.Item href="pages/yourVotedPosts.js">Voted posts</Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
 
-export default Sidebar;
+export default MenuSidebar;
