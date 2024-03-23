a14aaef9702217a863d015292cee2c8bf4149eb0
Erik Karsten <erik@opcode.xyz>
2024-03-23 17:03:19 +0200

    dunno: Add some broken tailwind

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Button, Input, Select, Space } from 'antd';
 import { createPost } from "../api";
 
 function CreatePost() {
@@ -25,29 +26,32 @@ function CreatePost() {
       .then((data) => {
         console.log("Board created:", data);
         alert("Board created!");
-        window.location.href = "/";
+        // window.location.href = "/";
       })
       .catch((error) => {
         alert("Error creating board!");
       });
   };
 
-  const locationWarning = <div>
-    <h1>Please enable location services</h1>
-  </div>
+  const locationWarning = (
+    <div>
+      <h1>Please enable location services</h1>
+    </div>
+  );
 
   if (!location.lat || !location.lon) {
     return locationWarning;
   }
 
   return (
-    <div>
-      <h1>Create New Post</h1>
-      <form onSubmit={submit}>
-        <input type="text" placeholder="Subject" />
-        <br />
-        <button>Submit Post</button>
-      </form>
+    <div className="create-card">
+    <div className="create-content">
+      <h1 className="block text-gray-700 text-sm font-bold mb-2">New Post</h1>
+        <Space.Compact style={{ width: '50%' }}>
+          <Input type="text" placeholder="Subject" />
+          <Button>Submit Post</Button>
+        </Space.Compact>
+      </div>
     </div>
   );
 }
