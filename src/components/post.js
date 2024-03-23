abf6db09c0fa071139c55bcb7b927b80f9a2dfe6
Erik Karsten <erik@opcode.xyz>
2024-03-23 09:41:14 +0200

    fix: remove placeholder image

--- src/components/post.js
+++ src/components/post.js
@@ -6,7 +6,6 @@ const Post = ({ post }) => {
     <div className="post-card">
       <div className="post-header">
         <span className="post-title">{post.title}</span>
-        <img src="https://via.placeholder.com/64" alt="placeholder" />
       </div>
       <div className="post-footer">
         <p className="post-location">{post.location}</p>
